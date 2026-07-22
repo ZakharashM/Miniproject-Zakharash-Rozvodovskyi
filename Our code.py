4a1e3c9222e84fc694b0b6915a7d4d1e68d40ac9
ZakharashM <mykhailo.zakharash.25@kse.org.ua>
2026-07-22 23:24:15 +0300

    Structurized the data into a data frame + calculated the average for 20 days and for 50 days from 2025-01-01 to 2025-12-31

--- Our code.py
+++ Our code.py
@@ -3,9 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-dani = yf.download("AAPL", start="2025-06-30", end="2025-07-20")
+dani = yf.download("AAPL", start="2024-06-01", end="2026-01-01")
 if isinstance(dani.columns, pd.MultiIndex):
     dani.columns = dani.columns.get_level_values(0)
 dani.dropna(inplace=True)
 dani = dani[['Open', 'High', 'Low', 'Close', 'Volume']]
+dani["MEAN20"] = dani["Close"].rolling(20).mean()
+dani["MEAN50"] = dani["Close"].rolling(50).mean()
+dani = dani.loc["2025-01-01":]
 print(dani)
