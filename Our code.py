52eb95fedf3576fbfaec72a4063cefa655b74a69
ZakharashM <mykhailo.zakharash.25@kse.org.ua>
2026-07-24 23:58:18 +0300

    Created a separate frame with all transactions(bought/sold) + calculated the PNL of our project

--- Our code.py
+++ Our code.py
@@ -11,4 +11,14 @@ dani = dani[['Open', 'High', 'Low', 'Close', 'Volume']]
 dani["MEAN20"] = dani["Close"].rolling(20).mean()
 dani["MEAN50"] = dani["Close"].rolling(50).mean()
 dani = dani.loc["2025-01-01":]
-print(dani)
+dani["Did we buy?"] = np.where(dani['MEAN20'] > dani['MEAN50'], 1, 0)
+dani["What to do?"] = dani["Did we buy?"].diff().fillna(0)
+kupivli = dani[dani["What to do?"] != 0][["Close", "MEAN20", "MEAN50", "Did we buy?", "What to do?"]]
+buys = dani[dani["What to do?"] == 1]["Close"]
+sells = dani[dani["What to do?"] == -1]["Close"]
+if sells.index[0] < buys.index[0]:
+    sells = sells.iloc[1:]
+n_trades = min(len(buys), len(sells))
+pnl = ((sells.iloc[:n_trades].values - buys.iloc[:n_trades].values)/ buys.iloc[:n_trades].values) * 1000
+print("Прибуток/збиток з кожної угоди ($):", pnl.round(2))
+print("Загальний фінансовий результат ($):", round(pnl.sum(), 2))
